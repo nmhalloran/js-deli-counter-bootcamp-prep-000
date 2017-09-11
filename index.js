d0fb0513294574b84d43cad909b95f7684c43881
Nicholas Halloran <nmhalloran@hotmail.com>
2017-09-11 00:00:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,18 +20,15 @@ function currentLine(line) {
   if (line.length === 0) {
     return "The line is currently empty."
   }
-  else {
-    var lineSentence = "The line is currently: "
-    for (var i = 0; i < line.length; i++) {
-      lineSentence += `${i + 1}. ${line[i]}`
-      if (i < line.length - 1) {
-        lineSentence += `, `
-      }
-    }
+
+  const numbersAndNames = []
+  
+  for (let i = 0, l = line.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
   }
-  console.log(lineSentence)
-  return lineSentence
-}
+
+  return `The line is currently: ${numbersAndNames.join(', ')}`
+};
 
 
 takeANumber(katzDeli, 'Ada')
